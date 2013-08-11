577c267bf1d7354f5e687d6a5cce2345c9f5a413
hueh <werikso@gmail.com>
2013-08-11 16:50:48 +0200

    Update montblanc.mk

--- montblanc.mk
+++ montblanc.mk
@@ -23,7 +23,6 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml \
 
 # Configs
 PRODUCT_COPY_FILES += \
